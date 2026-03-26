d4724b7ee66f9338f52998b612cf6fea58168407
Tên hoantd <hoantrandanh@gmail.com>
2026-03-26 08:58:11 +0700

    Tạo version 13

--- src/main/java/com/app/SpringBootApp.java
+++ src/main/java/com/app/SpringBootApp.java
@@ -1,7 +1,11 @@
 package com.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.app")
+@EnableJpaRepositories(basePackages = "com.app.repository")
+@EntityScan(basePackages = "com.app.model")
 public class SpringBootApp {
 }
